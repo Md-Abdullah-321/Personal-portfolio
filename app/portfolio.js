697684cb46b258c3a4099a11df4f7b4ae9ca5b02
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-23 23:19:47 +0600

    added respnsiveness

--- app/portfolio.js
+++ app/portfolio.js
@@ -20,13 +20,13 @@ export default function Portfolio() {
 
    
   return (
-    <div className="w-full min-h-[700px]  py-20 flex flex-col justify-center items-center" id="portfolio">
+    <div className="w-full h-[500px] sm:min-h-[700px]  py-20 flex flex-col justify-center items-center" id="portfolio">
      <div className="flex flex-col justify-center items-center w-full">
             <h2 className=" uppercase text-5xl sm:text-6xl md:text-8xl font-extrabold text-gray-200">Portfolio</h2>
             <h3 className="absolute text-3xl sm:text-4xl md:text-5xl uppercase font-extrabold md:mt-1.5 bg-gradient-to-r from-violet-600 to-violet-300 ... inline-block ... text-transparent ... bg-clip-text">my recent work</h3>
       </div>
 
-      <div className="w-full lg:w-5/6 mx-auto mt-20 px-5 lg:px-0">
+      <div className="w-full lg:w-5/6 mx-auto mt-20 px-2 sm:px-5 lg:px-0">
       {projects.length > 0 && <ImageSlider imageArray={projects}/>}
       </div>
     </div>
