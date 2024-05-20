ad2cc81b3373404754684bf46d16ac7f1ba565b7
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-05-20 18:06:37 +0600

    change logic and other things

--- app/admin/projects/page.js
+++ app/admin/projects/page.js
@@ -2,20 +2,28 @@
 
 import Sidebar from "@/components/sidebar";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Projects() {
-    const router = useRouter();
-    useEffect(() => {
-        //Temprary Solution:
-        if(!localStorage.getItem("User")){
-            return router.push('/admin/login', { scroll: false });
-        }
-    }, [])
+  const [screenWidth, setScreenWidth] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+      //Temprary Solution:
+      if(!localStorage.getItem("User")){
+          return router.push('/admin/login', { scroll: false });
+      }
+  }, []);
+
+  useEffect(() => {
+    const updateScreenWidth = () => setScreenWidth(window.innerWidth);
+    updateScreenWidth();
+    window.addEventListener('resize', updateScreenWidth);
+    return () => window.removeEventListener('resize', updateScreenWidth);
+  }, []);
     return (
       <div className="w-full min-h-screen flex justify-between">
-        <Sidebar/>
-        <div className="w-9/12 h-screen">Projects</div>
+         {screenWidth > 1024 && <Sidebar/>}
+        <div className="w-10/12 h-screen">Projects</div>
       </div>
     )
 }
