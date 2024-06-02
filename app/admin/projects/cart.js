ea5d250352413053dfc32b62b153ac672d155efb
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-06-02 17:10:59 +0600

    completed designing cart

--- app/admin/projects/cart.js
+++ app/admin/projects/cart.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
 
 const Cart = (project) => {
-    return <div className="h-80 w-60 shadow-md rounded-md">
-        <div className="h-48 w-full ">
-        <Image src={project?.projectImages[0]} alt="" srcSet="" className='w-full h-full object-cover object-center self-center' width={1920} height={1080}/>
+    console.log(project?.project?.title);
+    return <div className="h-[400px] w-72 shadow-md rounded-md bg-gray-50 border border-violet-200 flex flex-col justify-between">
+        <div className="h-[220px] w-full">
+        <Image src={project?.project?.projectImages[0]} alt="" srcSet="" className='w-full h-full object-cover object-center self-center rounded-br-full' width={1920} height={1080}/>
         </div>
-        <div className="flex flex-col justify-center h-32 p-2">
-            <h5 className="text-sm">Title: </h5>
-            <p>FrontEnd: {project?.frontEnd?.map((item, index) => <span key={index}>{item}</span>)}</p>
-            <p>Backend : {project?.backEnd?.map((item, index) => <span key={index}>{item}</span>)}</p>
+        <div className="flex flex-col justify-center h-40  p-2 text-[14px] gap-y-2">
+            <div className="w-full flex flex-col gap-y-1">
+                <h5 className="text-[15px] font-semibold uppercase text-violet-700">{project?.project?.title}</h5>
+                <p className="flex items-center gap-1 flex-wrap">FrontEnd: {project?.project?.frontEnd?.map((item, index) => index < 2 && <span key={index} className="bg-violet-200 mx-0.5 p-1 rounded-lg">{item}</span>)}</p>
+                <p className="flex items-center gap-1 flex-wrap">Backend : {project?.project?.backEnd?.map((item, index) => index < 2 && <span key={index} className="bg-violet-200  p-1 rounded-lg">{item}</span>)}</p>
+            </div>
 
             <div className="flex justify-between items-center mt-2">
                 <button className=" bg-violet-600 text-white text-sm px-4 py-1 uppercase rounded-sm hover:shadow-md">Edit</button>
--- components/sidebar.js
+++ components/sidebar.js
@@ -66,8 +66,8 @@ function Sidebar() {
 
       console.log(sidebar);
     return <>
-    {screenWidth < 1024 && !sidebar && <div className=' bg-violet-600 h-10 flex justify-center items-center w-32 sm:w-14 m-2 rounded-sm cursor-pointer' onClick={() => setSidebar(true)}><FaBars className='w-6 h-6 sm:w-8 sm:h-8 text-white'/></div>}
-    <div className={screenWidth < 1024 ? sidebar ? "show_sidebar w-[300px] h-screen bg-violet-600 flex flex-col absolute rounded-tr-lg rounded-br-lg": "sidebar_position w-[300px] h-screen bg-violet-600 flex flex-col absolute rounded-tr-lg rounded-br-lg": "w-[300px] h-screen bg-violet-600 flex flex-col rounded-tr-lg rounded-br-lg"}>
+    {screenWidth < 1024 && !sidebar && <div className=' bg-violet-600 h-10 flex justify-center items-center w-12 sm:w-14 m-2 rounded-sm cursor-pointer absolute' onClick={() => setSidebar(true)}><FaBars className='w-6 h-6 sm:w-8 sm:h-8 text-white'/></div>}
+    <div className={screenWidth < 1024 ? sidebar ? "show_sidebar w-[300px] h-screen bg-violet-600 flex flex-col absolute rounded-tr-lg rounded-br-lg z-50": "sidebar_position w-[300px] h-screen bg-violet-600 flex flex-col absolute rounded-tr-lg rounded-br-lg z-50": "w-[300px] h-screen bg-violet-600 flex flex-col rounded-tr-lg rounded-br-lg z-50"}>
         {/* header  */}
         <div className="h-40 md:h-60 w-full bg-white border-2 border-violet-600 shadow-md flex items-center justify-around rounded-lg">
         <div className='w-[90px] h-[90px] rounded-full border-2 border-violet-600 flex justify-center items-center'>
